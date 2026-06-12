40162cdb28b88d21eb29b9a15cf80dd1bc07af39
dbw-werke <gabrieltunjicc@gmail.com>
2026-06-12 13:22:06 +0200

    Add multiple product links for toom store

--- store-product-links.js
+++ store-product-links.js
@@ -1,5 +1,58 @@
 const STORE_PRODUCT_LINKS = {
   toom: {
-    perlfix: "https://toom.de/p/ansatzgips-perflix-25-kg/3053281?srsltid=AfmBOoq0asCDM8NYAD0RumAsx3I9OM3Ifv__s5pgKxAS-FEPyltFtFQ8"
+
+    perlfix:
+      "https://toom.de/p/ansatzgips-perflix-25-kg/3053281?srsltid=AfmBOoq0asCDM8NYAD0RumAsx3I9OM3Ifv__s5pgKxAS-FEPyltFtFQ8",
+
+    rotband:
+      "https://toom.de/p/haftputzgips-rotband-30-kg/3053468?srsltid=AfmBOopqX4H9r8PoctzquPHY1zuTr7QDITYkcWdYq-WF99NX7-kFVyD3",
+
+    uniflott:
+      "https://toom.de/p/spachtelmasse-uniflott-25-kg/3053754",
+
+    eckschiene:
+      "https://toom.de/p/duennputz-eckleiste-250-x-25-x-25-cm/3150752",
+
+    eckprofil:
+      "https://toom.de/p/duennputz-eckleiste-250-x-25-x-25-cm/3150752",
+
+    eckleiste:
+      "https://toom.de/p/duennputz-eckleiste-250-x-25-x-25-cm/3150752",
+
+    duennputzeckleiste:
+      "https://toom.de/p/duennputz-eckleiste-250-x-25-x-25-cm/3150752",
+
+    rigips:
+      "https://toom.de/p/ausbauplatte-60-x-125-x-200-cm/3251468",
+
+    regips:
+      "https://toom.de/p/ausbauplatte-60-x-125-x-200-cm/3251468",
+
+    rigipsplatte:
+      "https://toom.de/p/ausbauplatte-60-x-125-x-200-cm/3251468",
+
+    regipsplatte:
+      "https://toom.de/p/ausbauplatte-60-x-125-x-200-cm/3251468",
+
+    gipsplatte:
+      "https://toom.de/p/ausbauplatte-60-x-125-x-200-cm/3251468",
+
+    trockenbauplatte:
+      "https://toom.de/p/ausbauplatte-60-x-125-x-200-cm/3251468",
+
+    feuchtraumplatte:
+      "https://toom.de/p/gipskartonplatte-greenboard-200-x-60-x-125-cm/3254779",
+
+    badplatte:
+      "https://toom.de/p/gipskartonplatte-greenboard-200-x-60-x-125-cm/3254779",
+
+    grueneplatte:
+      "https://toom.de/p/gipskartonplatte-greenboard-200-x-60-x-125-cm/3254779",
+
+    gruneplatte:
+      "https://toom.de/p/gipskartonplatte-greenboard-200-x-60-x-125-cm/3254779",
+
+    greenboard:
+      "https://toom.de/p/gipskartonplatte-greenboard-200-x-60-x-125-cm/3254779"
   }
 };
