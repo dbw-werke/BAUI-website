344c340a4c3fe9c65a27b01f23dec4a7cf7fb1fa
dbw-werke <gabrieltunjicc@gmail.com>
2026-06-12 13:09:39 +0200

    Update store-product-links.js

--- store-product-links.js
+++ store-product-links.js
@@ -1,5 +1,5 @@
 const STORE_PRODUCT_LINKS = {
   toom: {
-    perlfix: "https://toom.de/p/ansatzgips-perflix-25-kg/3053281"
+    perlfix: "https://toom.de/p/ansatzgips-perflix-25-kg/3053281?srsltid=AfmBOoq0asCDM8NYAD0RumAsx3I9OM3Ifv__s5pgKxAS-FEPyltFtFQ8"
   }
 };
